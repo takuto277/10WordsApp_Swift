da83a312ba4d50da08d10910febab5c116221da3
Takuto <Takuto.72.soccer@gmail.com>
2023-02-13 22:26:42 +0900

    Initial Commit

---
+++ 10wordsApp_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
