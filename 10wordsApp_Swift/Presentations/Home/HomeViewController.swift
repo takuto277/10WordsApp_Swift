15efb6400699a5bd4fc04a905aa4aa3eb4eb1a82
Takuto <Takuto.72.soccer@gmail.com>
2023-02-14 01:47:15 +0900

    初回起動時、xibで起動する実装

---
+++ 10wordsApp_Swift/Presentations/Home/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/02/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBAction func quizStartButton(_ sender: Any){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
--- 10wordsApp_Swift/SceneDelegate.swift
+++ 10wordsApp_Swift/SceneDelegate.swift
@@ -17,6 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        self.window = window
+        window.makeKeyAndVisible()
+        
+        self.window?.rootViewController = HomeViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
