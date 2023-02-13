42b24c75c53025e4aeb6d24dda18cc63cdaa062a
Takuto <Takuto.72.soccer@gmail.com>
2023-02-14 02:02:02 +0900

    xibファイルの画面遷移実装

--- 10wordsApp_Swift/Presentations/Home/HomeViewController.swift
+++ 10wordsApp_Swift/Presentations/Home/HomeViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBAction func quizStartButton(_ sender: Any){
-        
+        //TODO: ViewControllerFactoryにて実装予定
+        let quizSelectViewController = QuizSelectViewController()
+        quizSelectViewController.modalPresentationStyle = .fullScreen
+        self.present(quizSelectViewController, animated: true)
     }
 
     override func viewDidLoad() {
---
+++ 10wordsApp_Swift/Presentations/QuizSelect/QuizSelectViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuizSelectViewController.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/02/14.
+//
+
+import UIKit
+
+class QuizSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
