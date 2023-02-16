3d413f8cb945a5ec1064a964f6fc027ba328ef04
Takuto <Takuto.72.soccer@gmail.com>
2023-02-17 01:38:10 +0900

    修正

---
+++ 10wordsApp_Swift/Models/InitialQuizWords.swift
@@ -0,0 +1,13 @@
+//
+//  InitialQuizWords.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/02/17.
+//
+
+import Foundation
+
+class InitialQuizWords {
+    //TODO: Arrayを取得する
+
+}
---
+++ 10wordsApp_Swift/Models/UserEditQuizWords.swift
@@ -0,0 +1,12 @@
+//
+//  UserEditWords.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/02/17.
+//
+
+import Foundation
+
+class UserEditQuizWords {
+    //TODO: Arrayを取得する
+}
