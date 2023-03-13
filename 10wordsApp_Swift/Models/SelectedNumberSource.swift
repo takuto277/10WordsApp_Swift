8b248b344ba0513b15d7c1195996c8712371353f
Takuto <Takuto.72.soccer@gmail.com>
2023-03-14 08:39:28 +0900

    Sourceの追加

---
+++ 10wordsApp_Swift/Models/SelectedNumberSource.swift
@@ -0,0 +1,8 @@
+//
+//  SelectedNumberSource.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/03/14.
+//
+
+import Foundation
---
+++ 10wordsApp_Swift/Models/SelectedNumberSourceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  SelectedNumberSourceProtocol.swift
+//  10wordsApp_Swift
+//
+//  Created by 小野拓人 on 2023/03/14.
+//
+
+import Foundation
